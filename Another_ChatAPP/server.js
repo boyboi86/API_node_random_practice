6b6c5b4bda5fc7170b5ef10a851aa354786e6f92
boyboi86 <boyboi86@gmail.com>
2016-11-01 20:44:37 +0800

    connected to sessions && router

--- Another_ChatAPP/server.js
+++ Another_ChatAPP/server.js
@@ -2,17 +2,18 @@
 
 const express = require('express');
 const app = express();
+/*index folder is the first folder to be called if non-specified*/
+const chat = require('./app/index');
 // const port = process.env.PORT || 3000
 app.set('port', process.env.PORT || 3000);
 app.set('view engine', 'ejs');
-/*Simple webserver that implements streams directs all assets towards public folder*/
-app.use(express.static('public'));
-const port = app.get('port')
 
-app.get('/', (req, res , next) => {
-  // res.sendFile(__dirname + '/views/login.htm');
-  res.render('login');
-});
+/*Simple webserver that implements streams directs all assets towards public folder*/
+app.use(express.static('public'))
+app.use('/', chat.session);
+app.use('/', chat.router);
+
+const port = app.get('port')
 
 app.listen(port, err => {
   err? console.error('err has occurred') : console.log(`server started on port ${port}`);
