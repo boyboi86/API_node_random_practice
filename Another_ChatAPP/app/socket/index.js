0b56f2ed33150c4781d177cb291ce0e18c16a9a5
boyboi86 <boyboi86@gmail.com>
2016-11-04 15:19:43 +0800

    created link for room validation and creation

--- Another_ChatAPP/app/socket/index.js
+++ Another_ChatAPP/app/socket/index.js
@@ -1,25 +1,29 @@
 'use strict';
 
+const helper = require('../helper');
+
 module.exports = (io, app) => {
   let allrooms = app.locals.chatrooms;
-
-  allrooms.push({
-    room: 'Good Food',
-    roomID: '0001',
-    users: []
-  })
-
-  allrooms.push({
-    room: 'Cloud Computering',
-    roomID: '0002',
-    users: []
-  })
   /*This is in charge of the namespace /roomslist in rooms.ejs to perform a handshake*/
   io.of('/roomslist').on('connection', socket => {
     /*When rooms.ejs emits 'getChatRoom' event it will perform a callback
       and serialize the list of chatroom and emits it back*/
     socket.on('getChatRoom', () => {
       socket.emit('chatRoomsList', JSON.stringify(allrooms));
+    })
+    socket.on('createNewRoom', newRoomInput => {
+      /*Check if current new room exists or not, if not create one*/
+      if(!helper.findRoomByName(allrooms, newRoomInput)){
+          allrooms.push({
+            room: newRoomInput,
+            roomID: helper.randomHex(),
+            user: []
+          })
+          /* Emit an updated list  to creator */
+          socket.emit('chatRoomsList', JSON.stringify(allrooms));
+          /*Emits an updated list to all open sockets*/
+          socket.broadcast.emit('chatRoomsList', JSON.stringify(allrooms));
+      }
     })
   })
 }
