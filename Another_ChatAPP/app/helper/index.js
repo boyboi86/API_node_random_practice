a53bcad27750fb1915688c7851013ad452fbc981
boyboi86 <boyboi86@gmail.com>
2016-11-05 17:56:54 +0800

    update methods

--- Another_ChatAPP/app/helper/index.js
+++ Another_ChatAPP/app/helper/index.js
@@ -1,175 +1,174 @@
 'use strict';
-
 const router = require('express').Router();
 const db = require('../db');
 const crypto = require('crypto');
 
-//iterate thru the above Objectify and perform a recursive method to call
+// Iterate through the routes object and mount the routes
 let _registerRoutes = (routes, method) => {
-  for (let key in routes){
-    if(typeof routes[key] === 'object' && routes[key] !== null && !(routes[key] instanceof Array)) {
-      //after fufilled criteria then go ahead and run
-      _registerRoutes(routes[key], key);
-    } else {
-      //register routes
-      if(method === 'get'){
-        router.get(key, routes[key]);
-      } else if(method === 'post'){
-        router.post(key, routes[key]);
-      } else {
-        router.use(routes[key]);
-      }
-    }
-  }
+	for(let key in routes) {
+		if(typeof routes[key] === 'object' && routes[key] !== null && !(routes[key] instanceof Array)) {
+			_registerRoutes(routes[key], key);
+		} else {
+			// Register the routes
+			if(method === 'get') {
+				router.get(key, routes[key]);
+			} else if(method === 'post') {
+				router.post(key, routes[key]);
+			} else {
+				router.use(routes[key]);
+			}
+		}
+	}
+
 }
 
 let route = routes => {
-  _registerRoutes(routes);
-  return router;
+	_registerRoutes(routes);
+	return router;
 }
 
-/*find a single user based on a key*/
+// Find a single user based on a key
 let findOne = profileID => {
-    return db.userModel.findOne({
-     'profileId': profileID
-  });
+	return db.userModel.findOne({
+		'profileId': profileID
+	});
 }
 
-/*photo is array always return 0 which would most likely be profile picture, otherwise use blank*/
+// Create a new user and returns that instance
 let createNewUser = profile => {
-  return new Promise((resolve, reject) => {
-    let newChatUser = new db.userModel({
-      profileId: profile.id,
-      fullName: profile.displayName,
-      profilePic: profile.photos[0].value || ""
-    });
-    newChatUser.save(error => {
-      if(error){
-        console.error('error saving profile');
-        reject(error);
-      } else {
-        resolve(newChatUser)
-      }
-    })
-  })
+	return new Promise((resolve, reject) => {
+		let newChatUser = new db.userModel({
+			profileId: profile.id,
+			fullName: profile.displayName,
+			profilePic: profile.photos[0].value || ''
+		});
+
+		newChatUser.save(error => {
+			if(error) {
+				reject(error);
+			} else {
+				resolve(newChatUser);
+			}
+		});
+	});
 }
 
+// The ES6 promisified version of findById
 let findById = id => {
-  return new Promise((resolve, reject) => {
-    db.userModel.findById(id, (error, user) => {
-      if(error){
-        reject(error);
-      } else {
-        resolve(user);
-      }
-    })
-  })
+	return new Promise((resolve, reject) => {
+		db.userModel.findById(id, (error, user) => {
+			if(error) {
+				reject(error);
+			} else {
+				resolve(user);
+			}
+		});
+	});
 }
-/*req.isAuthenticated is a method provided by passport for session that returns boolean*/
-/*This is to guard against trespassers, an in-build middleware*/
+
+// A middleware that checks to see if the user is authenticated & logged in
 let isAuthenticated = (req, res, next) => {
-  // console.log(req.isAuthenticated())
-  // console.log(req);
-  if(req.isAuthenticated()){
-    next()
-  } else {
-    res.redirect('/');
-  }
+	if(req.isAuthenticated()) {
+		next();
+	} else {
+		res.redirect('/');
+	}
 }
 
-/* create a method to search through the entire list of roomNames in allrooms and see it exists */
-let findRoomByName = (allrooms, rooms) => {
-  let findRoom = allrooms.findIndex((element, index, array) => {
-    if(element.rooms === rooms) {
-      return true;
-    } else {
-      return false;
-    }
-    /*if it is greater than 0 it means room exists,
-      this is a simple validator because if you use lowercase or trim validtion will still be false*/
-    return findRoom > -1 ? true: false;
-  })
+// Find a chatroom by a given name
+let findRoomByName = (allrooms, room) => {
+	let findRoom = allrooms.findIndex((element, index, array) => {
+		if(element.room === room) {
+			return true;
+		} else {
+			return false;
+		}
+	});
+	return findRoom > -1 ? true : false;
 }
 
-/* generate a random roomID*/
+// A function that generates a unique roomID
 let randomHex = () => {
-  return crypto.randomBytes(24).toString('Hex');
+	return crypto.randomBytes(24).toString('hex');
 }
 
-/* find room by id */
+// Find a chatroom with a given ID
 let findRoomById = (allrooms, roomID) => {
-  return allrooms.find((element, index, array) => {
-    if(element.roomID === roomID){
-      return true;
-    } else {
-      return false;
-    }
-  })
+	return allrooms.find((element, index, array) => {
+		if(element.roomID === roomID) {
+			return true;
+		} else {
+			return false;
+		}
+	});
 }
 
-/* Add a user to chatroom */
+// Add a user to a chatroom
 let addUserToRoom = (allrooms, data, socket) => {
-  /* Get the room subject */
-  let getRoom = findRoomById(allrooms, data.roomID);
-  if(getRoom !== undefined){
-    /* Get active user ID*/
-    /* Previously we were using express-session
-      when we use io.use so we already expose to session and can 'borrow' session from express*/
-    let userID = socket.request.session.passport.user
+	// Get the room object
+	let getRoom = findRoomById(allrooms, data.roomID);
+	if(getRoom !== undefined) {
+		// Get the active user's ID (ObjectID as used in session)
+		let userID = socket.request.session.passport.user;
+		// Check to see if this user already exists in the chatroom
+		let checkUser = getRoom.users.findIndex((element, index, array) => {
+			if(element.userID === userID) {
+				return true;
+			} else {
+				return false;
+			}
+		});
 
-    let checkUser = getRoom.users.findIndex((element, index, array) => {
-      if(element.userID === userID) {
-        return true
-      } else {
-        return false
-      }
-    })
-    /* If the user already exist in the room, remove user first */
-    if(checkUser > -1){
-      getRoom.users.splice(checkUser, 1);
-    }
-    // Push user into user array
-    getRoom.users.push({
-      socketID: socket.id,
-      userID,
-      user: data.user,
-      userPic: data.userPic
-    });
-    /*Join room channel again*/
-    socket.join(data.roomID);
+		// If the user is already present in the room, remove him first
+		if(checkUser > -1) {
+			getRoom.users.splice(checkUser, 1);
+		}
 
-    /*update user list*/
-    return getRoom;
-  }
+		// Push the user into the room's users array
+		getRoom.users.push({
+			socketID: socket.id,
+			userID,
+			user: data.user,
+			userPic: data.userPic
+		});
+
+		// Join the room channel
+		socket.join(data.roomID);
+
+		// Return the updated room object
+		return getRoom;
+	}
 }
 
+// Find and purge the user when a socket disconnects
 let removeUserFromRoom = (allrooms, socket) => {
-  for(let room of allrooms){
-    let findUser = room.users.findIndex((element, index, array) => {
-      if(element.socketID === socket.id){
-        return true;
-      } else {
-        return false;
-      }
-      if(findUser > -1){
-        socket.leave(room.roomID);
-        room.users.splice(findUser, 1);
-        return room;
-      }
-    })
-  }
-}
+	for(let room of allrooms) {
+		// Find the user
+		let findUser = room.users.findIndex((element, index, array) => {
+			if(element.socketID === socket.id) {
+				return true;
+			} else {
+				return false;
+			}
+		});
 
+		if(findUser > -1) {
+			socket.leave(room.roomID);
+			room.users.splice(findUser, 1);
+			return room;
+		}
+	}
+}
 
 module.exports = {
-  route,
-  findOne,
-  createNewUser,
-  findById,
-  isAuthenticated,
-  findRoomByName,
-  randomHex,
-  findRoomById,
-  addUserToRoom,
-  removeUserFromRoom
+	route,
+	findOne,
+	createNewUser,
+	findById,
+	isAuthenticated,
+	findRoomByName,
+	randomHex,
+	findRoomById,
+	addUserToRoom,
+	removeUserFromRoom
 }
