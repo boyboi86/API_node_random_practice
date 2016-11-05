a14e939381400d5558ff073e39dd86d15788a1a5
boyboi86 <boyboi86@gmail.com>
2016-11-05 17:57:44 +0800

    update broadcast with user leave chatroom

--- Another_ChatAPP/app/socket/index.js
+++ Another_ChatAPP/app/socket/index.js
@@ -1,47 +1,55 @@
 'use strict';
-
 const helper = require('../helper');
 
 module.exports = (io, app) => {
-  let allrooms = app.locals.chatrooms;
-  /*This is in charge of the namespace /roomslist in rooms.ejs to perform a handshake*/
-  io.of('/roomslist').on('connection', socket => {
-    /*When rooms.ejs emits 'getChatRoom' event it will perform a callback
-      and serialize the list of chatroom and emits it back*/
-    socket.on('getChatRoom', () => {
-      socket.emit('chatRoomsList', JSON.stringify(allrooms));
-    })
-    socket.on('createNewRoom', newRoomInput => {
-      /*Check if current new room exists or not, if not create one*/
-      if(!helper.findRoomByName(allrooms, newRoomInput)){
-          allrooms.push({
-            room: newRoomInput,
-            roomID: helper.randomHex(),
-            users: []
-          })
-          /* Emit an updated list  to creator */
-          socket.emit('chatRoomsList', JSON.stringify(allrooms));
-          /*Emits an updated list to all open sockets*/
-          socket.broadcast.emit('chatRoomsList', JSON.stringify(allrooms));
-      }
-    })
-  })
+	let allrooms = app.locals.chatrooms;
 
-  io.of('/chatter').on('connection', socket => {
-    /* Join a chatroom */
-    socket.on('join', data => {
-      let usersList = helper.addUserToRoom(allrooms, data, socket);
-      /* Update a list of user  */
-        socket.broadcast.to('data.roomID').emit('updateUsersList', JSON.stringify(usersList.users));
-        socket.emit('updateUsersList', JSON.stringify(usersList.users));
-    })
+	io.of('/roomslist').on('connection', socket => {
+		socket.on('getChatrooms', () => {
+			socket.emit('chatRoomsList', JSON.stringify(allrooms));
+		});
 
-    /*on disconnect, you will need to refresh the page*/
-    socket.on('disconnect', () => {
-      /*find the room and purge user that exit chat room*/
-      let room = removeUserFromRoom(allrooms, socket);
-      socket.broadcast.to(room.roomID).emit('updateUsersList', JSON.stringify(room.users));
-    })
+		socket.on('createNewRoom', newRoomInput => {
+			// check to see if a room with the same title exists or not
+			// if not, create one and broadcast it to everyone
+			if(!helper.findRoomByName(allrooms, newRoomInput)) {
+				// Create a new room and broadcast to all
+				allrooms.push({
+					room: newRoomInput,
+					roomID: helper.randomHex(),
+					users: []
+				});
 
-  })
+				// Emit an updated list to the creator
+				socket.emit('chatRoomsList', JSON.stringify(allrooms));
+				// Emit an updated list to everyone connected to the rooms page
+				socket.broadcast.emit('chatRoomsList', JSON.stringify(allrooms));
+			}
+		});
+	});
+
+
+	io.of('/chatter').on('connection', socket => {
+		// Join a chatroom
+		socket.on('join', data => {
+			let usersList = helper.addUserToRoom(allrooms, data, socket);
+
+			// Update the list of active users as shown on the chatrooom page
+			socket.broadcast.to(data.roomID).emit('updateUsersList', JSON.stringify(usersList.users));
+			socket.emit('updateUsersList', JSON.stringify(usersList.users));
+		});
+
+		// When a socket exits
+		socket.on('disconnect', () => {
+			// Find the room, to which the socket is connected to and purge the user
+			let room = helper.removeUserFromRoom(allrooms, socket);
+			socket.broadcast.to(room.roomID).emit('updateUsersList', JSON.stringify(room.users));
+		});
+
+		// When a new message arrives
+		socket.on('newMessage', data => {
+			socket.to(data.roomID).emit('inMessage', JSON.stringify(data));
+		});
+	});
+
 }
